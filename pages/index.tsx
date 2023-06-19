1b813a6c9dd516daa1bee284ad0af835b0e20c9b
satang_budsai <satang_budsai@cmu.ac.th>
2023-06-19 14:47:57 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,44 @@
-import Layout from "@/components/Layout";
+import { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import Layout from "@/components/Layout";
 
 export default function Home() {
   const router = useRouter();
-
-  const handleInstall = () => {
-    if (typeof window !== "undefined" && "share" in navigator) {
-      navigator.share({
-        title: "App Title",
-        text: "Check out this app!",
-        url: window.location.href,
-      })
-        .then(() => console.log("App shared successfully"))
-        .catch((error) => console.error("Error sharing app:", error));
-    }
-  };
+  const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
 
   useEffect(() => {
-    const handleInstallPrompt = () => {
-      // Hide the install button for unsupported devices
-      const installButton = document.getElementById("installApp");
-      if (installButton) {
-        installButton.style.display = "none";
-      }
+    const handleBeforeInstallPrompt = (event: any) => {
+      event.preventDefault();
+      setDeferredPrompt(event);
     };
 
-    window.addEventListener("click", handleInstallPrompt);
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
 
     return () => {
-      window.removeEventListener("click", handleInstallPrompt);
+      window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
     };
   }, []);
+
+  const handleInstall = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the installation prompt");
+        } else {
+          console.log("User dismissed the installation prompt");
+        }
+        setDeferredPrompt(null);
+      });
+    }
+  };
 
   return (
     <Layout className="p-5">
       <div className="uppercase">Template AroundTheCode</div>
       <Button variant="contained" onClick={() => router.push('/pageDemo')}>Home</Button>
-      <Button id="installApp" variant="contained" className="ml-2" onClick={handleInstall}>
+      <Button variant="contained" className="ml-2" onClick={handleInstall}>
         Install App
       </Button>
     </Layout>
